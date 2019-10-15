f507314abab0cef6861aca07d8d23c72f8179c99
KATRINA CHLOE REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESKA@OOCL.COM>
2019-10-15 21:13:36 +0800

    Create Function for Add, Remove, Update and retrieve

--- src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeResource.java
@@ -1,27 +1,64 @@
 package com.tw.apistackbase.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tw.apistackbase.model.Employee;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.random;
+import static java.lang.String.valueOf;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
-public class HelloResource {
+@RequestMapping("/employees")
+public class EmployeeResource {
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
+    private List<Employee> employeeList = new ArrayList();
 
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
-
-        return ResponseEntity.ok("Hello:" + userName);
+    @GetMapping(path = "")
+    public List<Employee> getAll() {
+        return employeeList;
     }
 
+    @PostMapping(path = "")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        int employeeID = parseInt(valueOf(new Random().nextInt()));
 
+        employee.setId(employeeID);
+
+        employeeList.add(employee);
+        return employee;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public Employee deleteEmployee(@PathVariable int id){
+        Employee deleteEmployee = employeeList.stream()
+                .filter(employee -> employee.getId() == id)
+                .findFirst()
+                .orElse(null);
+        employeeList.remove(deleteEmployee);
+        return deleteEmployee;
+    }
+
+    @PutMapping(path = "/{id}")
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee){
+        Employee updateEmployee = employeeList.stream()
+                .filter(emp -> emp.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        employee.setId(id);
+
+        employeeList.remove(updateEmployee);
+
+        employeeList.add(employee);
+        return updateEmployee;
+    }
 }
